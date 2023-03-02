1976bc081f085b0e4443845b693a8eed6dee30f1
loiPH VNCODER <37902965+loiphamvncoder@users.noreply.github.com>
2023-03-02 10:27:24 +0700

    Create git-error-fix.php

---
+++ git-error-fix.php
@@ -0,0 +1,5 @@
+Error 1: 
+git rebase origin/development the following error message is shown from Git:
+fatal: refusing to merge unrelated histories
+Error redoing merge 1234deadbeef1234deadbeef
+--allow-unrelated-histories
